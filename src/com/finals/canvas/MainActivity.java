1e7e1469c11a98653ace8c470fb447bcdcba541b
yiyunde <695826705@qq.com>
2015-01-31 17:30:56 +0800

    风微风微风而我

---
+++ src/com/finals/canvas/MainActivity.java
@@ -0,0 +1,17 @@
+package com.finals.canvas;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.SeekBar;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+}
